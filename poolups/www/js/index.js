022a2f5e1bc2e241ca4d657022d2d38674e9ad35
Vivi <viviana@sutedjo.net>
2018-08-16 21:31:28 +0200

    prog

--- poolups/www/js/index.js
+++ poolups/www/js/index.js
@@ -1,7 +1,33 @@
 
+var firstStart = true
 
-$(document).on("deviceready", (event) => {
+function pickdate(){
+    let options = {
+        date: new Date(),
+        mode: 'datetime'
+    }
+    let onSuccess = function (date){
+        var hours = calculateHours(date)
+        $("#pooltime").html(hours)
+        if(firstStart) {
+            $("#date_button").removeClass("centered")
+            $("#pooltime_container").removeClass("hidden")
+            firstStart=false
+        }
+        alert('Selected date: '+ date)
+    }
 
+    let onError = function(error){
+        alert('Error: '+ error)
+    }
+    datePicker.show(options,onSuccess,onError);
+}
 
-
-});
+function calculateHours(date){
+    var nowTime = Date.now()
+    var endTime = date.value
+    var diff = endTime-nowTime
+    var days = diff/(1000*60*60*24)
+    var hours = days*6
+    return hours
+}
