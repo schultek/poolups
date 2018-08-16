fa247d7e4a600df16b75c410164a94a5c9d12d4d
kilian <kilian@4xschulte.de>
2018-08-16 23:15:34 +0400

    app structure

--- poolups/platforms/android/assets/www/cordova_plugins.js
+++ poolups/platforms/android/assets/www/cordova_plugins.js
@@ -1,10 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-plugin-datepicker.DatePicker",
+    "file": "plugins/cordova-plugin-datepicker/www/android/DatePicker.js",
+    "pluginId": "cordova-plugin-datepicker",
+    "clobbers": [
+      "datePicker"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-browsersync": "0.1.7"
+  "cordova-plugin-browsersync": "0.1.7",
+  "cordova-plugin-datepicker": "0.9.3"
 };
 // BOTTOM OF METADATA
 });
--- poolups/platforms/browser/www/cordova_plugins.js
+++ poolups/platforms/browser/www/cordova_plugins.js
@@ -4,7 +4,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-browsersync": "0.1.7"
+    "cordova-plugin-browsersync": "0.1.7",
+    "cordova-plugin-datepicker": "0.9.3"
 }
 // BOTTOM OF METADATA
 });
